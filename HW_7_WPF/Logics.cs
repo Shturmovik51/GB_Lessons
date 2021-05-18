6da101f3c8b2e530764d9baa3545a8f4d8f59446
Shturmovik <Radist-KPS@Yandex.ru>
2021-05-19 02:52:25 +0300

    ДЗ 7 на проверку

---
+++ HW_7_WPF/Logics.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace HW_7_WPF
+{
+    class Logics
+    {
+        public TextBox txt;
+        public Button buttonStart, buttonEnter;
+        public TextBlock bestCountTXT;
+        public Label bubble, countlbl;
+        public Image img1, img2, img3, img4;
+        int memory, count;
+        
+        
+        public async void Generator()
+        {
+            buttonStart.IsEnabled = false;
+            img1.Visibility = Visibility.Hidden;
+            img4.Visibility = Visibility.Hidden;
+            img2.Visibility = Visibility.Visible;            
+            await Task.Delay(400);
+            bubble.Content = ".";
+            await Task.Delay(400);
+            bubble.Content = ". .";
+            await Task.Delay(400);
+            bubble.Content = ". . .";
+            await Task.Delay(400);
+            Random rand = new Random();
+            memory = rand.Next(1, 10);
+            bubble.Content = "Загадал";
+            txt.IsEnabled = true;
+            buttonEnter.IsEnabled = true;
+            img2.Visibility = Visibility.Hidden;
+            img3.Visibility = Visibility.Visible;
+        }
+
+        public int Comparator(int number)
+        {
+            count++;
+            countlbl.Content = count;
+            txt.Text = string.Empty;
+            return memory.CompareTo(number);
+        }
+
+        public string Messenger(int result)
+        {
+            if (result < 0) return "Мое меньше";
+            else if (result > 0) return "Мое больше";
+            else
+            {
+                img3.Visibility = Visibility.Hidden;
+                img4.Visibility = Visibility.Visible;
+                buttonStart.IsEnabled = true;
+                txt.IsEnabled = false;
+                buttonEnter.IsEnabled = false;
+                countlbl.Content = string.Empty;
+
+                if (bestCountTXT.Text == "пока нет" || count < int.Parse(bestCountTXT.Text)) bestCountTXT.Text = count.ToString();
+
+                count = 0;
+                return "Вы угадали";
+            }
+                
+        }
+
+    }
+}
---
+++ HW_7_WPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_7_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Logics logics = new Logics();        
+        public MainWindow()
+        {
+            InitializeComponent();
+            logics.txt = txt;
+            logics.bubble = bubble;
+            logics.countlbl = count;
+            logics.bestCountTXT = bestCount;
+            logics.img1 = img1;
+            logics.img2 = img2;
+            logics.img3 = img3;
+            logics.img4 = img4;
+            logics.buttonStart = buttonStart;
+            logics.buttonEnter = buttonEnter;
+        }
+            
+
+        private void ButtonEnterNum(object sender, RoutedEventArgs e)
+        {
+            bubble.Content = logics.Messenger(logics.Comparator(int.Parse(txt.Text))); 
+        }
+
+        private void ButtonGenerateNum(object sender, RoutedEventArgs e)
+        {
+            logics.Generator();
+        }
+    }
+}
